c1ad4991124bf788b1cbeeef6621825725c39535
Sabuj Jana <sabujlovescoding@gmail.com>
2021-09-03 19:20:46 +0530

    Create DiceCombinations.cpp

---
+++ Dynamic Programming/DiceCombinations.cpp
@@ -0,0 +1,52 @@
+/*
+Sabuj Jana / @greenindia
+https://www.janasabuj.github.io
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define crap ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long int
+#define double long double
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+#define endl "\n"
+void print1d(const vector<int>& vec) {for (auto val : vec) {cout << val << " ";} cout << endl;}
+void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto val : row) {cout << val << " ";} cout << endl;}}
+const int mod = 1e9 + 7;
+const int N = 1e6 + 7;
+int n;
+int dp[N];
+void solve() {
+	cin >> n;
+	dp[0] = 1;
+
+	for (int x = 1; x <= n; x++) {
+		for (int i = 1; i <= 6; i++) {
+			if (x - i >= 0)
+				dp[x] = (dp[x] + dp[x - i]) % mod;
+		}
+	}
+
+	cout << dp[n] << endl;
+}
+
+#define SABUJ_JANA_WxF 1
+signed main() {
+	crap;
+#ifdef SABUJ_JANA_WF
+	auto start = chrono::high_resolution_clock::now();
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout); freopen("error.txt", "w", stderr);
+#endif
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+#ifdef SABUJ_JANA_WF
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+	cerr << "Time taken :\n" << duration.count() / 1000000.0 << "s" << "\n";
+#endif
+	return 0;
+}
